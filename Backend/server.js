eea6dd2efd33908e1c8aeddc796a634911351b28
PRAVESH TIWARI <pravesht252@gmail.com>
2026-04-10 15:53:10 +0530

    Backend

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+// DB connect
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// server
+app.listen(5000, () => console.log("Server running on port 5000"));
